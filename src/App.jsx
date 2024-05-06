87a9aab0f8e6baab8d9a4c2f06498036a282dc28
Tabitha <tabitha.r.mccracken@gmail.com>
2024-05-06 15:19:51 -0600

    File Setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header.jsx'
+import Content from './components/Content.jsx'
+import Footer from './components/Footer.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+      <Content color="blue" text="This is my first React Application!" />
+      <Content color="red" text="Wish me luck..." />
+      <Content color="green" text="I think I've got it!" />
+      <Footer />
+    </>
+  )
+}
+
+export default App
